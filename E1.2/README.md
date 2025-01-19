da0cc48ed22657dba07c3a0e3d65ce4d668da686
Kevin Nguyen <legendaryricefarmer@gmail.com>
2025-01-19 04:35:18 -0500

    FiveMessages

---
+++ E1.2/README.md
@@ -0,0 +1,11 @@
+## Pseudocode
+
+FUNCTION main
+SET message TO "Hello, World!"
+Set repeat TO 5
+SET i TO 0
+
+WHILE i IS LESS THAN repeat
+    PRINT message
+    INCREMENT 1 BY 1
+END FUNCTION
---
+++ E1.2/src/FiveMessages.java
@@ -0,0 +1,12 @@
+public class FiveMessages {
+    public static void main(String[] args) {
+        String message = "Hello, World!";
+        int repeat = 5;
+        int i = 0;
+        
+        while (i < repeat) {
+            System.out.println(message);
+            i++;
+        }
+    }
+}
