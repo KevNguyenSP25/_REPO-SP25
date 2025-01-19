d37750089708326fb7185798ebebbcfda504fa42
Kevin Nguyen <legendaryricefarmer@gmail.com>
2025-01-19 15:12:21 -0500

    E1.1Update3

--- E1.1/3Messages/src/Message.java
+++ E1.1/3Messages/src/Message.java
@@ -1,6 +1,6 @@
-public class App {
+public class Message {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java");
         System.out.println("Welcome to Computer Science");
         System.out.println("Programming is fun");
     }
